3a275d48fa11d86a174e86994ec6a76b4804e716
RomVP456 <momanrom456@gmail.com>
2024-04-07 00:51:05 -0600

    Solved Bug data persistence
    Solved bug where the current player name cannot be changed

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine;
 
 public class SceneManagement : MonoBehaviour
 {
     // Start is called before the first frame update
+    // public string playerName;
+    [SerializeField] TMP_InputField nameField;
+
     void Start()
     {
         
@@ -14,5 +18,10 @@ public class SceneManagement : MonoBehaviour
     // Update is called once per frame
     public void ChangeLevel(){
         SceneManager.LoadScene(1);
+    }
+    public void setPlayerName(){
+        string playerName = nameField.text;
+        // print(playerName);
+        DataManager.DataInstance.playerName = playerName;
     }
 }
